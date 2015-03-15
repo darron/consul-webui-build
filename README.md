996fcd4ce2950108b3221978676d901791d3cefd
Darron Froese <darron@froese.org>
2015-03-14 19:22:46 -0600

    First commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+Consul Web UI Build
+============
+
+This repository automatically builds Consul Web UI packages and pushes them to my Packagecloud Repository.
+
+To use the repository - take a look [here](https://packagecloud.io/darron/consul-webui/install) or:
+
+`curl https://packagecloud.io/install/repositories/darron/consul-webui/script.deb | sudo bash`
---
+++ fpm-recipes/consul-webui/recipe.rb
@@ -0,0 +1,31 @@
+class ConsulWebui < FPM::Cookery::Recipe
+  name 'consul-webui'
+
+  version '0.5.0'
+  revision '1'
+  description 'Admin web UI for Consul.'
+
+  homepage 'https://github.com/hashicorp/consul/tree/master/ui'
+  source "https://dl.bintray.com/mitchellh/consul/#{version}_web_ui.zip"
+  sha256 '0081d08be9c0b1172939e92af5a7cf9ba4f90e54fae24a353299503b24bb8be9'
+
+  maintainer 'Darron Froese <darron@froese.org>'
+  vendor 'octohost'
+
+  license 'Mozilla Public License, version 2.0'
+
+  depends 'consul'
+  provides 'consul-webui'
+  build_depends 'unzip'
+
+  pre_install 'preinst'
+
+  def build
+    safesystem 'echo "Nothing to build - it is html."'
+  end
+
+  def install
+    safesystem "mkdir -p #{destdir}/var/lib/consul/ui"
+    safesystem "cp -ar #{builddir}/#{version}_web_ui/dist/* #{destdir}/var/lib/consul/ui/"
+  end
+end
