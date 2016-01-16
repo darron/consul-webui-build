65e0d18345e39decd9385b8e64b32b961ee44c33
darron froese <darron@froese.org>
2016-01-16 15:16:57 -0700

    Update to 0.6.3

--- fpm-recipes/consul-webui/recipe.rb
+++ fpm-recipes/consul-webui/recipe.rb
@@ -1,13 +1,13 @@
 class ConsulWebui < FPM::Cookery::Recipe
   name 'consul-webui'
 
-  version '0.6.1'
+  version '0.6.3'
   revision '1'
   description 'Admin web UI for Consul.'
 
   homepage 'https://github.com/hashicorp/consul/tree/master/ui'
   source "https://releases.hashicorp.com/consul/#{version}/consul_#{version}_web_ui.zip"
-  sha256 'afccdd540b166b778c7c0483becc5e282bbbb1ee52335bfe94bf757df8c55efc'
+  sha256 '93bbb300cacfe8de90fb3bd5ede7d37ae6ce014898edc520b9c96a676b2bbb72'
 
   maintainer 'Darron Froese <darron@froese.org>'
   vendor 'octohost'
